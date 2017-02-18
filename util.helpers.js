b60ff781be81e79267a8b7bcae9d39960e4a9d6c
Manuel Wiedenmann <manuel@funkensturm.de>
2017-02-18 15:31:47 +0100

    Feat(util.helpers): Add rememberTo and rememberToFor

--- util.helpers.js
+++ util.helpers.js
@@ -1,4 +1,49 @@
 module.exports = {
+  rememberTo(callback, condition, options) {
+    const { key, limit } = options;
+
+    // Helper
+    const reset = function() { this.memory[key] = 0; };
+
+    // Setup
+    if (!this.memory[key]) { reset.call(this); }
+
+    // Increase if condition is met or reset
+    if (condition) { this.memory[key] += 1; }
+    else { reset.call(this); }
+
+    // If the limit es reached call callback and reset
+    if (this.memory[key] > limit) {
+      callback.call(this);
+
+      reset.call(this);
+    }
+  },
+
+  rememberToFor(callback, condition, options) {
+    const { key, id, limit } = options;
+
+    // Helper
+    const reset = function() { this.memory[key][id] = 0; };
+
+    // Setup
+    if (!this.memory[key]) { this.memory[key] = {}; }
+    if (!this.memory[key][id]) { reset.call(this); }
+
+    // Increase if condition is met or reset
+    if (condition) { this.memory[key][id] += 1; }
+    else { reset.call(this); }
+
+    // console.log(JSON.stringify(this.memory[key][id]));
+
+    // If the limit es reached call callback and reset
+    if (this.memory[key][id] > limit) {
+      callback.call(this);
+
+      reset.call(this);
+    }
+  },
+
   // Exec callback every X ticks
   everyTicks(amount, callback) {
     if (Game.time % amount === 0) {
